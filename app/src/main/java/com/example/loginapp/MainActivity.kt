21bd720825cdf798f9492cac205a2326ecffdfe8
abhimanyukumarsingh123 <akturki11122004@gmail.com>
2024-08-15 11:51:58 -0700

    Demo Login page

---
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.loginapp
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loginapp.ui.theme.LoginAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    innerPadding
+                    LoginUi()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun LoginUi(modifier: Modifier = Modifier) {
+    val context : Context= LocalContext.current
+
+    val email = remember {
+        mutableStateOf("")
+    }
+
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    Column {
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "Welcome to My Frist ", fontSize = 26.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Image(
+
+            painter = painterResource(id = R.drawable.login),
+
+            contentDescription = null,
+            modifier = Modifier
+                .height(250.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+
+        OutlinedTextField(
+            value = email.value, onValueChange = {
+                email.value=it
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            placeholder = {
+                Text(text = "E-mail")
+            }
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        OutlinedTextField(
+            value =password.value, onValueChange = {
+                password.value=it
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            placeholder = {
+                Text(text = "Password")
+            }
+
+        )
+        Spacer(modifier = Modifier.height(70.dp))
+        OutlinedButton(
+            onClick = {
+                Toast.makeText(context,"You have Login Seccess ful",Toast.LENGTH_SHORT).show()
+            },
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            Text(text = "Login")
+        }
+    }
+}
